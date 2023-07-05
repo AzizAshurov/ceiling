656bc81f3a71b14c22738a1d772a1df6dc42d2ac
AzizAshurov <aziz.ashurov44@gmail.com>
2023-07-05 11:30:57 +0500

    стилизую квиз

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const quiz = new Swiper('.quiz__swiper', {
+        pagination: {
+            el: ".qiuz__pagination",
+            type: "fraction",
+          },
+        autoHeight: true,
+        navigation: {
+          nextEl: '.quiz__button-next',
+          prevEl: '.quiz__button-prev',
+        },
+      });
+})
+  
+  
