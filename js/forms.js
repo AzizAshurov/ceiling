1e13d17223ee93f6428ae2598569f151a7b3e097
AzizAshurov <aziz.ashurov44@gmail.com>
2023-07-16 17:09:58 +0500

    делаем js отправки форм

--- js/forms.js
+++ js/forms.js
@@ -1,62 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function handleFormSubmit(event) {
-        // Просим форму не отправлять данные самостоятельно
-        event.preventDefault()
-        console.log('Отправка!')
-        event.preventDefault()
-        serializeForm(applicantForm)
-    }
+  async function handleFormSubmit(event) {
+    event.preventDefault()
+  
+    const data = serializeForm(event.target)
+    const response = await sendData(data)
+  }
+  
 
-    function serializeForm(formNode) {
-        console.log(formNode.elements)
+  function serializeForm(formNode) {
+    return new FormData(formNode)
+  }
+  
+  async function sendData(data) {
+    return await fetch('/ceiling/php/mail.php/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'multipart/form-data' },
+      body: data,
+    })
+  }
+  
+  
 
-        const { elements } = formNode
-
-        const data = Array.from(elements)
-            .filter((item) => !!item.name)
-            .map((element) => {
-            const { name, value } = element
-
-            return { name, value }
-            })
-
-        console.log(data)
-
-        Array.from(elements)
-            .forEach((element) => {
-            const { name, value } = element
-            console.log({ name, value })
-        })
-        
-        return new FormData(formNode)
-        
-        console.log(Array.from(data.entries()))
-      }
-      
-      async function sendData(data) {
-        return await fetch(window.location.origin + '/mail.php', {
-          method: 'POST',
-          headers: { 'Content-Type': 'multipart/form-data' },
-          body: data,
-        })
-      }
-
-    async function handleFormSubmit(event) {
-        event.preventDefault()
-      
-        const data = serializeForm(event.target)
-        const response = await sendData(data)
-      }
-
-
-
-
-
-    const applicantForm = document.getElementById('lead__form')
-    applicantForm.addEventListener('submit', handleFormSubmit)
-      
-      
-      
+  const applicantForm = document.getElementById('lead__form')
+  applicantForm.addEventListener('submit', handleFormSubmit)
+  
+  
 })
 
 
