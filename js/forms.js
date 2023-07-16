91173f7ab8697b0387bad803dcc7a979ef87c284
AzizAshurov <aziz.ashurov44@gmail.com>
2023-07-17 00:12:24 +0500

    Делает универсальный обработчик форм

--- js/forms.js
+++ js/forms.js
@@ -1,30 +1,62 @@
 document.addEventListener('DOMContentLoaded', function () {
-  async function handleFormSubmit(event) {
-    event.preventDefault()
+  const forms = document.querySelectorAll('.js_form');
   
-    const data = serializeForm(event.target)
-    const response = await sendData(data)
-  }
-  
+  async function handleSubmitForm(event) {
+    event.preventDefault();
 
-  function serializeForm(formNode) {
-    return new FormData(formNode)
-  }
-  
+    // Берем текущую форму и кнопки отправки внутри нее
+    const formNode = event.target;
+    const submitButton = formNode.querySelector('button[type="submit"]');
+    const submitButtonTextNode = submitButton.querySelector('.btn__text');
+
+    // Запоминаем исходный текст кнопки
+    const initialButtonText = submitButton.innerText;
+    
+    // Делаем кнопку некликабельной и меняем текст
+    submitButton.classList.add('btn--loading');
+    submitButtonTextNode.innerText = 'Отправляем...';
+
+    //Осбираем данные из формы и отправляем их в mail.php
+    const formData = new FormData(formNode)
+    const response = await sendData(formData);
+
+    // Меняем текст кнопки на галочку
+    submitButtonTextNode.innerText = '✓';
+
+    // Ждем 15 секунд, меняем текст кнопки на исходный и делаем кнопку кликабельной
+    setTimeout(() => {
+      submitButtonTextNode.innerText = initialButtonText;
+      submitButton.classList.remove('btn--loading');
+    }, 15000);
+
+    // Чистим поля формы
+    formNode.reset();
+
+    /*
+    ✓  0. При отправке формы заменить текст кнопки на «отправвляем» и сделать ее некликабельной.
+    ✓    1. Запомнить иходный текст кнопки
+    ✓  1. Получить все значения инпутов из форм
+    ✓  2. отправить значения инпутов из формы на mail.php
+    ✓  3. меняем текст кнопки на галочку ✓ на 15 секунд, а после возвращаем исходный текст кнопки
+    ✓  4. очистим форму после успешной отправки
+    ✓  5. отправим в яндекс мтерику событие отправки формы
+    */
+  }  
+
   async function sendData(data) {
-    return await fetch('/ceiling/php/mail.php/', {
+    ym(94322465,'reachGoal','submit-form-thanks')
+    const response = await fetch(window.location.origin + '/ceiling/php/mail.php', {
       method: 'POST',
-      headers: { 'Content-Type': 'multipart/form-data' },
       body: data,
-    })
-  }
-  
-  
+    });
 
-  const applicantForm = document.getElementById('lead__form')
-  applicantForm.addEventListener('submit', handleFormSubmit)
-  
-  
+    return response;
+  }
+
+  function handleForms(form) {
+    form.addEventListener('submit', handleSubmitForm);
+  }
+  forms.forEach(handleForms);
 })
 
 
--- php/mail.php
+++ php/mail.php
@@ -1,74 +1,352 @@
 <?php
 
-$message_name = 'Заявки';
-$message_email = 'aziz.ashurov31@yandex.ru';
+// Перед тем, как орать почему заявка не отправляется, проверь этот массив
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Получаем все поля из формы и файл, если он есть
-  $fields = $_POST;
-  $file = $_FILES['user_file'];
+$config = array(
+    'email' => false,
+    'telegram' => false,
+    'google_sheets' => false,
+    'logfile' => false,
+    'amo' => false,
+	'wirecrm' => false,
+);
 
-  // Задаем email адреса получателей
-  $to = explode_textarea(carbon_get_theme_option('leads_emails'));
-  $to = implode(',', $to);
+/**
+ * @date 2021-10-18
+ * @param {string} $var_name
+ * @returns {any}
+ */
+function getSafeValue ($var_name) {
+    $safe_value = strip_tags($_GET[$var_name]);
+    $safe_value = htmlentities($safe_value, ENT_QUOTES, "UTF-8");
+    $safe_value = htmlspecialchars($safe_value, ENT_QUOTES);
+    return $safe_value;
+}
 
-  // Заголовки письма
-  $subject = 'Новое сообщение с сайта';
-  $headers = array(
-    'From: ' . $message_name . ' <' . $message_email . '>',
-    'Reply-To: ' . 'info@animation-school.ru',
-    'Content-Type: text/html; charset=UTF-8',
-    // 'multipart' => true,
-    // 'Content-Type: multipart/mixed; boundary=boundary'
-  );
+function getActualLink() {
+    return 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+}
 
-  // Создаем тело письма
-  $message = '<html><body>';
-  foreach ($fields as $key => $value) {
-    if ($key !== 'user_file') {
-      $message .= '<p><strong>' . ucfirst($key) . ':</strong> ' . $value . '</p>';
+function getCurrentDate() {
+    $date = new DateTime();
+    $date = $date->format('d.m.Y, H:i');
+    return $date;
+}
+
+?>
+
+
+<?php
+/**
+ * EMAIL
+ */
+// configs
+
+if ($config['email']) {
+
+    $formname = getSafeValue('formname');
+    $name = getSafeValue('user_name');
+    $tel = getSafeValue('user_tel');
+
+    $actual_link = getActualLink();
+
+    $message_body =
+        "<a href=\"$actual_link\">$actual_link</a><br/><br/>" .
+        "Форма: $formname<br/><br/>" .
+        "Имя: $name<br/><br/>" .
+        "Телефон: $tel<br/><br/>";
+
+	$email_to = '=== === Куда отправлять === ===';
+	$email_from = '=== === От кого отправлять === ===';
+	$email_subject = "=== === Тема письма === ===";
+
+    $msg_box = "";
+    $errors = array();
+
+    // если форма без ошибок
+    if(empty($errors)){
+        // собираем данные из формы
+        $message = "Имя: " . $_POST['user_name'] . "<br/> Телефон: " . $_POST['user_tel'];
+        send_mail($message, $email_to, $email_subject, $email_from); // отправим письмо
     }
-  }
-  $message .= '</body></html>';
 
-  // Если файл был загружен, добавляем его как вложение
-  if ($file['size'] > 0) {
-    $file_name = $file['name'];
-    $file_type = $file['type'];
+    // функция отправки письма
+    function send_mail($message, $email_to, $email_subject, $email_from){
 
-    // Получаем расширение файла из MIME-типа
-    $mime_types = wp_get_mime_types();
-    $ext = array_search($file_type, $mime_types, true);
-    if (!$ext) {
-      // Если не удалось получить расширение из MIME-типа, пытаемся получить его из имени файла
-      $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+
+        // почта, на которую придет письмо
+        $mail_to = $email_to;
+
+        // тема письма
+        $subject = $email_subject;
+
+        // заголовок письма
+        $headers= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+        $headers .= "From: <". $email_from .">\r\n"; // от кого письмо
+
+        // отправляем письмо
+        mail($mail_to, $subject, $message, $headers);
     }
+}
+?>
 
-    // Генерируем имя файла с расширением
-    $file_name_with_ext = basename($file_name, '.' . $ext) . '.' . $ext;
 
-    // Создаем массив вложений
-    $attachments = array(
-      'name' => $file_name_with_ext,
-      'type' => $file_type,
-      'tmp_name' => $file['tmp_name'],
-      'error' => $file['error'],
-      'size' => $file['size']
+<?php
+/**
+ * Telegram
+ */
+
+if ($config['telegram']) {
+    //В переменную $token нужно вставить токен, который нам прислал @botFather
+    $token = "";
+
+    //Сюда вставляем chat_id
+    $chat_id = "";
+
+    //Определяем переменные для передачи данных из нашей формы
+    // if ($_POST['act'] == 'order') {
+        $name = ($_POST['user_name']);
+        $phone = ($_POST['user_tel']);
+    // }
+
+    //Собираем в массив то, что будет передаваться боту
+        $arr = array(
+            'Имя:' => $name,
+            'Телефон:' => $phone
+        );
+
+    //Настраиваем внешний вид сообщения в телеграме
+        foreach($arr as $key => $value) {
+            $txt .= "<b>".$key."</b> ".$value."%0A";
+        };
+
+    //Передаем данные боту
+        $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+}
+?>
+
+
+<?php
+/**
+ * GOOGLE SPREADSHEETS
+ */
+
+if ($config['google_sheets']) {
+    // формируем запись в таблицу google (изменить)
+    $url = "https://docs.google.com/forms/d/ссылка_на_твой_опросник/formResponse";
+    // массив данных (изменить entry, draft и fbzx)
+    $post_data = array (
+        "entry.286082292" => $_POST['user_name'],
+        "entry.1033324795" => $_POST['user_tel'],
+        "draftResponse" => "[,,&quot;2360450121179784330&quot;]",
+        "pageHistory" => "0",
+        "fbzx" => "-2360450121179784330"
     );
-  } else {
-    $attachments = array();
-  }
 
-  $result = array();
+    // Далее не трогать
+    // с помощью CURL заносим данные в таблицу google
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // указываем, что у нас POST запрос
+    curl_setopt($ch, CURLOPT_POST, 1);
+    // добавляем переменные
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+    //заполняем таблицу google
+    $output = curl_exec($ch);
+    curl_close($ch);
 
-  // Отправляем письмо через wp_mail()
-  if (wp_mail($to, $subject, $message, $headers, $attachments)) {
-    $result['status'] = 'ok';
-    // echo 'Сообщение успешно отправлено';
-  } else {
-    $result['status'] = 'fail';
-    // echo 'Ошибка при отправке сообщения';
-  }
+}
+?>
 
-  echo json_encode($result);
+<?php
+if ($config['logfile']) {
+
+    $log_file_name = 'leads.log';
+
+    $fp = fopen($log_file_name, 'a+');
+
+    $date = date('Y-m-d H:i:s') . "\n";
+
+
+    $formname = getSafeValue('formname');
+    $name = getSafeValue('user_name');
+    $tel = getSafeValue('user_tel');
+
+    $actual_link = getActualLink();
+
+    $log_data =
+        "$actual_link\n" .
+        "Форма: $formname\n" .
+        "Имя: $name\n" .
+        "Телефон: $tel\n";
+
+    $text = $date . "\n" .
+        $log_data . "\n" .
+        "---------\n\n";
+
+    fwrite($fp, $text);
+    fclose($fp);
+
+}
+?>
+
+<?php
+
+if ($config['amo']) {
+    // AMO lead
+    $paramsArray = array(
+        'fields[note_2]' => '',
+        // This parameters you can find at from publishing section → Wordpress shortcode
+        'form_id' => '1172670',
+        'hash'    => 'ba77a2b0ebdd4c553b453c6f51319aa6'
+    ); 
+
+    if (isset($_POST['user_name'])) {
+        $paramsArray['name_1'] = $_POST['user_name'];
+    } else {
+        $paramsArray['name_1'] = '';
+    }
+    if (isset($_POST['user_tel'])) {
+        $paramsArray['fields[419093_1][816111]'] = $_POST['user_tel'];
+    } else {
+        $paramsArray['fields[419093_1][816111]'] = '';
+    }
+
+    foreach ($_POST as $key => $value) {
+        $paramsArray['fields[note_2]'] .= $key." ".$value."\n";
+    };
+
+    $vars = http_build_query($paramsArray);
+
+    $options = array(
+        'http' => array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $vars
+        )
+    );
+
+    $send_counter = 0;
+
+    $context  = stream_context_create($options);
+    $amo = file_get_contents('https://forms.amocrm.ru/queue/add', false, $context);
+}
+
+if ($config['wirecrm']) {
+	//Пример добавления сделки с указанием определенного этапа продажи на PHP
+	$apikey = "ВСТАВЬ СЮДА СВОЙ АПИ";
+	//Получаем список этапов сделок
+	$get_deals_url = "https://wirecrm.com/api/v1/deals/stages";
+
+	$headers = array("X-API-KEY:".$apikey);
+
+	$handle = curl_init();
+
+	curl_setopt($handle, CURLOPT_URL, $get_deals_url);
+
+	curl_setopt($handle, CURLOPT_USERAGENT, "WireCRM Rest API");
+
+	curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+
+	curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+	$data = curl_exec($handle);
+
+	curl_close($handle);
+
+	$all_deals_json = json_decode($data);
+
+	$today = date("Y-m-d");
+
+	$description = "";
+
+	foreach ($_POST as $post) {
+		if ($post === $_POST['user_name'] || $post === $_POST['user_tel']) break;
+			$description .= "{$post}\n\n";
+	}
+
+	//Добавляем сделку с первым этапом из списка выше
+	$deal = array(
+		'name' => $_POST['user_name'],
+		'description' => $description,
+		'currency' => '1',
+		'stage' => $all_deals_json->data[0]->id,
+		'd_close' => $today,
+		// 'stage' => 0,
+	);
+	$deal_url = "https://wirecrm.com/api/v1/deals";
+
+	$contact = array(
+		'name' => $_POST['user_name'],
+		'phone' => $_POST['user_tel'],
+		'description' => $description
+	);
+	$contact_json = json_encode($contact);
+	$contact_url = 'https://wirecrm.com/api/v1/contacts';
+
+		// $handle = curl_init();
+		// curl_setopt($handle, CURLOPT_URL, $deal_url);
+		// curl_setopt($handle, CURLOPT_USERAGENT, "WireCRM Rest API");
+		// curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+		// curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+		// curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "POST");
+		// curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+		// curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+		// $data = curl_exec($handle);
+		// curl_close($handle);
+		// var_dump($data);
+
+	function wirecrm_post($url, $headers, $data) {
+		$handle = curl_init();
+		curl_setopt($handle, CURLOPT_URL, $url);
+		curl_setopt($handle, CURLOPT_USERAGENT, "WireCRM Rest API");
+		curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($handle, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+		$data = curl_exec($handle);
+		curl_close($handle);
+
+		return json_decode($data);
+	}
+	function wirecrm_get($url, $headers, $data) {
+		$handle = curl_init();
+		curl_setopt($handle, CURLOPT_URL, $url);
+		curl_setopt($handle, CURLOPT_USERAGENT, "WireCRM Rest API");
+		curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+		$data = curl_exec($handle);
+		curl_close($handle);
+
+		return json_decode($data);
+	}
+
+	// $deals = wirecrm_get($get_deals_url, $headers, $data);
+
+	$new_contact = wirecrm_post($contact_url, $headers, $contact_json);
+
+	$deal['contact'] = $new_contact->data;
+	if (isset($_POST['bundle_price'])) {
+		$price = str_replace(' ', '', $_POST['bundle_price']);
+		$price = preg_replace('/\D/', ' ', $price);
+		$deal['price'] = $price;
+	}
+	$deal_json = json_encode($deal);
+
+	$new_deal = wirecrm_post($deal_url, $headers, $deal_json);
+
+	print_r('{}');
+
 }
